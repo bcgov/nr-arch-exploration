a235320e4ed3ac41f89f381667b2e96c88150ea3
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-04-06 19:03:21 -0700

    initial commit for vue code.

---
+++ uis/vue-sample/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useAppState } from '../stores/appState'
+
+defineProps<{ msg: string }>()
+
+const appState = useAppState();
+</script>
+
+<template>
+</template>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+
+label {
+    margin: 0 0.5em;
+    font-weight: bold;
+}
+
+code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+}
+</style>
---
+++ uis/vue-sample/src/router/routes.ts
@@ -0,0 +1,9 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] =
+    [{
+        path: '/',
+        component: () => import('../layouts/MainLayout.vue'),
+    }];
+
+export default routes;
