2e05cbad5a979a0ce31e19182d1420e30cd9381d
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-04-20 10:43:11 -0700

    update quarkus version and pipeline

--- apis/Go/fiber/v1/routes/employee-routes.go
+++ apis/Go/fiber/v1/routes/employee-routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func employeeRoutes(app fiber.Router) {
+	r := app.Group("/api/v1")
+	r.Get("/employees", GetEmployees)
+	r.Get("/employees/:id", GetEmployee)
+	r.Post("/employees", PostEmployee)
+	r.Put("/employees/:id", PutEmployee)
+	r.Delete("/employees/:id", DeleteEmployee)
+}
