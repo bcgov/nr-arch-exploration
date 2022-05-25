c1b94c767c96008f90b74fedf9d19ed50f957f90
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-05-24 18:35:48 -0700

    Add certificates conditionally

--- Metabase/openshift/startup.sh
+++ Metabase/openshift/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-java InstallCert --quiet "${DB_HOST}:${DB_PORT}"
-keytool -exportcert -alias "$DB_HOST-1" -keystore jssecacerts -storepass changeit -file /opt/oracle.cer
-keytool -importcert -alias orakey -noprompt -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file /opt/oracle.cer
-java -jar metabase.jar
+if [ -z "${DB_HOST}" ]; then
+  echo no DB_HOST provided starting metabase
+  java -jar metabase.jar
+else
+  echo adding certificates for "${DB_HOST}:${DB_PORT}"
+  java InstallCert --quiet "${DB_HOST}:${DB_PORT}"
+  keytool -exportcert -alias "$DB_HOST-1" -keystore jssecacerts -storepass changeit -file /opt/oracle.cer
+  keytool -importcert -alias orakey -noprompt -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file /opt/oracle.cer
+  java -jar metabase.jar
+fi
